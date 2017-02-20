7fc81fd016c712fad2c1efd43628652cb8ae12eb
Liam-O <postLiamO@gmail.com>
2017-02-20 17:28:38 -0500

    added new R data file

--- Week3/all_PLUTO_data.R
+++ Week3/all_PLUTO_data.R
@@ -1,3 +1,6 @@
+#Liam Byrne
+# Data 608 - Week 3
+
 # ***Working Diretory must have folder 'BORO_zip_files_csv' within***
 
 BK <- read.csv("BORO_zip_files_csv\\BK.csv")
@@ -6,3 +9,9 @@ MN <- read.csv("BORO_zip_files_csv\\MN.csv")
 QN <- read.csv("BORO_zip_files_csv\\QN.csv")
 SI <- read.csv("BORO_zip_files_csv\\SI.csv")
 
+NYC <- merge(BK, BX, all =TRUE)
+NYC <- merge(NYC, MN, all =TRUE)
+NYC <- merge(NYC, QN, all =TRUE)
+NYC <- merge(NYC, SI, all =TRUE)
+
+save(NYC, file="NYC_all.Rda")

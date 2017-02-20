fd6706e8eadc2d7b90dbaab248763d3c4250e2ee
Liam-O <postLiamO@gmail.com>
2017-02-20 18:06:50 -0500

    updated data file

--- Week3/all_PLUTO_data.R
+++ Week3/all_PLUTO_data.R
@@ -1,15 +1,15 @@
 #Liam Byrne
 # Data 608 - Week 3
 
-BK <- read.csv("https://media.githubusercontent.com/media/Liam-O/Data608/master/Week3/BORO_zip_files_csv/BK.csv")
-BX <- read.csv("https://media.githubusercontent.com/media/Liam-O/Data608/master/Week3/BORO_zip_files_csv/BX.csv")
-MN <- read.csv("https://media.githubusercontent.com/media/Liam-O/Data608/master/Week3/BORO_zip_files_csv/MN.csv")
-QN <- read.csv("https://media.githubusercontent.com/media/Liam-O/Data608/master/Week3/BORO_zip_files_csv/QN.csv")
-SI <- read.csv("https://media.githubusercontent.com/media/Liam-O/Data608/master/Week3/BORO_zip_files_csv/SI.csv")
+BK <- read.csv("https://github.com/Liam-O/Data608/blob/master/Week3/BORO_zip_files_csv/BK.csv?raw=true")
+BX <- read.csv("https://github.com/Liam-O/Data608/blob/master/Week3/BORO_zip_files_csv/BX.csv?raw=true")
+MN <- read.csv("https://github.com/Liam-O/Data608/blob/master/Week3/BORO_zip_files_csv/MN.csv?raw=true")
+QN <- read.csv("https://github.com/Liam-O/Data608/blob/master/Week3/BORO_zip_files_csv/QN.csv?raw=true")
+SI <- read.csv("https://github.com/Liam-O/Data608/blob/master/Week3/BORO_zip_files_csv/SI.csv?raw=true")
 
 NYC <- merge(BK, BX, all =TRUE)
 NYC <- merge(NYC, MN, all =TRUE)
 NYC <- merge(NYC, QN, all =TRUE)
 NYC <- merge(NYC, SI, all =TRUE)
 
-save(NYC, file="NYC_pluto_all.csv")
+write.csv(NYC, file="NYC_pluto_all.csv")

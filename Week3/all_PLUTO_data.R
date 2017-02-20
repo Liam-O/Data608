73f74d3456e37c97bd1b2242ff8ffc561121a098
Liam-O <postLiamO@gmail.com>
2017-02-19 21:15:03 -0500

    R load file

---
+++ Week3/all_PLUTO_data.R
@@ -0,0 +1,8 @@
+# ***Working Diretory must have folder 'BORO_zip_files_csv' within***
+
+BK <- read.csv("BORO_zip_files_csv\\BK.csv")
+BX <- read.csv("BORO_zip_files_csv\\BX.csv")
+MN <- read.csv("BORO_zip_files_csv\\MN.csv")
+QN <- read.csv("BORO_zip_files_csv\\QN.csv")
+SI <- read.csv("BORO_zip_files_csv\\SI.csv")
+

7b77914ec53f4d1ccddac2397845ca0b0e4bc39d
Liam-O <postLiamO@gmail.com>
2017-05-17 20:14:42 -0400

    Final Upload

--- FinalPro/server.R
+++ FinalPro/server.R
@@ -1,40 +1,70 @@
-# library(dplyr)
-# library(tidyr)
-# library(leaflet)
-# library(rgdal)
-#
-# #Shape files needed to be stored locally. To download, go to
-# #https://github.com/Liam-O/Data608/tree/master/FinalPro/ZillowNeighborhoods-NY
-#
-# nyc_geo <- readOGR(dsn = "ZillowNeighborhoods-NY", layer = "ZillowNeighborhoods-NY",
-#                    GDAL1_integer64_policy = TRUE)
-#
-# #Subset and rearrange object
-# boroughs <- c("New York", "Kings", "Richmond", "Bronx", "Queens")
-# nyc_geo <- subset(nyc_geo, City == "New York" & County %in% boroughs)
-# colnames(nyc_geo@data) <- c("State", "Borough", "City", "Neighborhood", "RegionID")
-# nyc_geo@data <- as.data.frame(apply(nyc_geo@data, MARGIN = 2, as.character), stringsAsFactors = FALSE)
-# nyc_geo@data$RegionID <- as.numeric(nyc_geo@data$RegionID)
-# #Remove duplicates from odd areas
-# rem_shp <- c(110, 133, 153,177)
-# nyc_geo<- nyc_geo[!(rownames(nyc_geo@data) %in% rem_shp),]
-#
-# #Bring in nyc data
-# nyc_data <- read.csv("https://github.com/Liam-O/Data608/blob/master/FinalPro/nyc_all.csv?raw=true",
-#                      stringsAsFactors = FALSE)
-# nyc_data <- left_join(nyc_data, nyc_geo@data, by = c("Borough", "Neighborhood"))
-# nyc_data <- na.omit(nyc_data)
-# nyc_data$Date <- as.Date(nyc_data$Date)
-#
-# indicators <- read.csv("https://raw.githubusercontent.com/Liam-O/Data608/master/FinalPro/nyc_indicators.csv",
-#                        stringsAsFactors = FALSE)
-#
-# limits <- bbox(nyc_geo)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(leaflet)
+library(rgdal)
+library(plotly)
+
+#Shape files needed to be stored locally. To download, go to
+#https://github.com/Liam-O/Data608/tree/master/FinalPro/ZillowNeighborhoods-NY
+
+nyc_geo <- readOGR(dsn = "ZillowNeighborhoods-NY", layer = "ZillowNeighborhoods-NY",
+                   GDAL1_integer64_policy = TRUE)
+
+#Subset and rearrange object
+boroughs <- c("New York", "Kings", "Richmond", "Bronx", "Queens")
+nyc_geo <- subset(nyc_geo, City == "New York" & County %in% boroughs)
+colnames(nyc_geo@data) <- c("State", "Borough", "City", "Neighborhood", "RegionID")
+nyc_geo@data <- as.data.frame(apply(nyc_geo@data, MARGIN = 2, as.character), stringsAsFactors = FALSE)
+nyc_geo@data$RegionID <- as.numeric(nyc_geo@data$RegionID)
+#Remove duplicates from odd areas
+rem_shp <- c(110, 133, 153,177)
+nyc_geo<- nyc_geo[!(rownames(nyc_geo@data) %in% rem_shp),]
+
+#Bring in nyc data
+nyc_data <- read.csv("https://github.com/Liam-O/Data608/blob/master/FinalPro/nyc_all.csv?raw=true",
+                     stringsAsFactors = FALSE)
+nyc_data <- left_join(nyc_data, nyc_geo@data, by = c("Borough", "Neighborhood"))
+nyc_data <- na.omit(nyc_data)
+nyc_data$Date <- as.Date(nyc_data$Date)
+
+indicators <- read.csv("https://raw.githubusercontent.com/Liam-O/Data608/master/FinalPro/nyc_indicators.csv",
+                       stringsAsFactors = FALSE)
+
+limits <- bbox(nyc_geo)
 
 function(input, output, session) {
 
-    #Subset data based on input
+    check_data <- function(x) {
+        if (all(is.na(x))) {
+            showModal(modalDialog(
+                title = "Invalid Entry",
+                "No data available for current parameters.")
+                )
+            FALSE
+        }
+        else NULL
+    }
 
+    #Get plot data
+    getPlotData <- reactive({
+
+        hood <- click_data$clickedShape
+
+        if(is.null(hood)) {
+            hood <- 272816
+            }
+
+        nyc_plot <- nyc_data %>%
+            filter((indicator == input$indic) &
+                       (RegionID == hood)
+            )
+        click_data$clickedShape <- hood
+
+        nyc_plot
+    })
+
+    #Subset data based on input
     getSubData <- reactive({
         nyc_dynam <- nyc_data %>%
             filter((indicator == input$indic) &
@@ -71,7 +101,7 @@ function(input, output, session) {
         nyc_map@data <- left_join(nyc_map@data, nyc_dynam, by = "RegionID")
 
         nyc_map
-        })
+    })
 
     # Render map on run and set origin view
     output$nycMap <- renderLeaflet({
@@ -80,9 +110,6 @@ function(input, output, session) {
             setView(lng = mean(limits[1,]), lat = mean(limits[2,]), zoom = 10)
         })
 
-    #Set original map limits for view
-
-
     # Indicator Selector
     output$indicSelector <- renderUI ({
         indic_lst <- sort(indicators$Indicator.Code)
@@ -90,11 +117,29 @@ function(input, output, session) {
                     choices = indic_lst, selected = "All Homes")
     })
 
+    click_data <- reactiveValues(clickedShape = NULL)
+    observeEvent(input$nycMap_shape_click, {
+        click_data$clickedShape <- input$nycMap_shape_click$id
+    })
+
+    output$plot <- renderPlotly({
+        plot_ly(getPlotData(), x = ~Date, y = ~value,
+                type = "scatter", mode = "lines") %>%
+            layout(title = paste0(input$indic, "<br>for ",
+                                nyc_geo$Neighborhood[nyc_geo$RegionID == click_data$clickedShape][1],
+                ", ", nyc_geo$Borough[nyc_geo$RegionID == click_data$clickedShape][1])
+                )
+        })
 
     observe({
         data_render <- getSubData()
 
         #create color pallete for map
+        # Check that there is data for parameters
+        validate(
+            check_data(data_render@data$val)
+        )
+
         pal <- colorNumeric("RdBu",
                             domain = data_render@data$val,
                             na.color = "#bdbdbd")
@@ -105,10 +150,8 @@ function(input, output, session) {
                           "<strong> Neighborhood: </strong>",
                           data_render$Neighborhood, ", ", data_render$Borough, "<br>",
                           "<strong>", input$span, " Year(s) ", input$stat, ":</strong> ",
-                          ifelse(
-                              indic_symbol == "$",
-                              paste0(indic_symbol,data_render$val),
-                              paste0(data_render$val, indic_symbol))
+                          ifelse(indic_symbol == "$", indic_symbol,""),
+                          data_render$val, ifelse(indic_symbol == "%", indic_symbol, "")
                           )
 
         #Render on change
@@ -123,10 +166,11 @@ function(input, output, session) {
                         highlightOptions = highlightOptions(
                             color = "white", weight = 2,
                             bringToFront = TRUE),
+                        layerId = data_render$RegionID,
                         popup = nyc_pop) %>%
             clearControls() %>%
-            addLegend(pal = pal, values = data_render$val,
-                      position = "bottomright",
+            addLegend(pal = pal, values = data_render@data$val,
+                      position = "bottomleft",
                       title = paste0(input$indic, ifelse(indic_symbol %in% c("$", "%"),
                                                          paste0(" (In ", indic_symbol, ")"), "")
                                      )
--- FinalPro/ui.R
+++ FinalPro/ui.R
@@ -1,52 +1,47 @@
 library(shinydashboard)
 library(leaflet)
-library(DT)
+library(plotly)
 
 #Get indicator options
 indicators <- read.csv("https://raw.githubusercontent.com/Liam-O/Data608/master/FinalPro/nyc_indicators.csv",
                        stringsAsFactors = FALSE)
 boroughs <- c("New York", "Kings", "Richmond", "Bronx", "Queens")
 
-bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("nycMap", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-                  selectInput("boro", "NYC Borough",
-                              choices = c("All", sort(boroughs)),
-                              selected = "All"),
-                  selectInput("indic", "Real Estate Indicators",
-                              choices = sort(indicators$Indicator.Code),
-                              selected = "All Homes"),
-                  selectInput("stat", "Statistic",
-                              choices = (c("min", "max", "mean", "median")),
-                              selected = "max"),
-                  selectInput("span", "Years Spanned",
-                              choices = c(1, 2, 4, 8),
-                              selected = 8)
+header <- dashboardHeader(title = "NYC Real Estate")
+
+body <- dashboardBody(
+    fluidRow(
+        br(),
+        column(width = 9,
+               box(width = NULL, solidHeader = TRUE,
+                   leafletOutput("nycMap", height = 750)
+                   )
+               ),
+        column(width = 3,
+               box(width = NULL,
+                   selectInput("boro", "NYC Borough",
+                               choices = c("All", sort(boroughs)),
+                               selected = "All"),
+                   selectInput("indic", "Real Estate Indicators",
+                               choices = sort(indicators$Indicator.Code),
+                               selected = "All Homes"),
+                   selectInput("stat", "Statistic",
+                               choices = (c("min", "max", "mean", "median")),
+                               selected = "max"),
+                   selectInput("span", "Years Spanned",
+                               choices = c(1, 2, 4, 8),
+                               selected = 8)
+               ),
+               box(width = NULL,
+                   plotlyOutput("plot")
+               )
+        )
+
     )
 )
 
-# header <- dashboardHeader(title = "New York City Real Estate Indicators")
-#
-# body <- dashboardBody(
-#     fluidRow(
-#         column(width = 9,
-#                box(width = NULL, solidHeader = TRUE,
-#                leafletOutput("nycMap", height = 400)
-#                )
-#               ),
-#         column(width = 3,
-#            box(width = NULL,
-#                uiOutput("indicSelector"),
-#                uiOutput("spanSelector"),
-#                uiOutput("statSelector")
-#                )
-#            )
-#     )
-# )
-#
-# dashboardPage(
-#     header,
-#     dashboardSidebar(disable = TRUE),
-#     body
-# )
+dashboardPage(
+    header,
+    dashboardSidebar(disable = TRUE),
+    body
+)
